cac63da058ea92b73657b3f9b9d95f7452cdbd76
unknown <neetamourya24@gmail.com>
2021-12-05 15:00:26 +0530

    [Neeta mourya] Day9 Address Book System Display Message

---
+++ src/main/java/Day9/AddressBookSystem/AddressBookSystemDisplayMessage.java
@@ -0,0 +1,8 @@
+package Day9.AddressBookSystem;
+
+public class AddressBookSystemDisplayMessage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System");
+	}
+
+}
