a167e3d96076ec87a0b520ba189925b653edeb9d
unknown <neetamourya24@gmail.com>
2021-12-06 08:50:34 +0530

    [Neeta mourya] Game simulator

--- src/main/java/Day7/Review/GamblerSimulator.java
+++ src/main/java/Day7/Review/GamblerSimulator.java
@@ -1,7 +1,16 @@
 package Day7.Review;
 
 public class GamblerSimulator {
-	int start=100;
-	int bets=1;
-
+	int trials=100;
+    int	goal=200;
+    int bets=1;
+    int wins=0;
+    public static void main(String[] args) {
+    	int choice=0;
+		int bet= (int)Math.floor(Math.random()*10)%2;
+		if(bet==choice)
+			System.out.println("win");
+		else
+			System.out.println("loose");    	
+	}
 }
