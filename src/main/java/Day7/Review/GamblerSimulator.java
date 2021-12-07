38203bf3cb665bdb90670e26ea9316b0b1d6a376
unknown <neetamourya24@gmail.com>
2021-12-07 22:25:41 +0530

    [Neeta mourya] Welcome message

--- src/main/java/Day7/Review/GamblerSimulator.java
+++ src/main/java/Day7/Review/GamblerSimulator.java
@@ -1,13 +1,14 @@
 package Day7.Review;
 
 public class GamblerSimulator {
+	static int stake=100;
+    int bets=1;
 
     public static void main(String[] args) {
     	GamblerSimulator gamblerSimulator=new GamblerSimulator();
     	gamblerSimulator.startStake();
     }
     	public static void startStake() {    		
-    		int stake=100;
-    	    int bets=1;
+    		
 	}
 }
---
+++ src/main/java/Day9/AddressBookSystem/AddressBookSystem.java
@@ -0,0 +1,7 @@
+package Day9.AddressBookSystem;
+
+public class AddressBookSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System");
+	}
+}
